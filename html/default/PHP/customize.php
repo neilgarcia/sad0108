9d89bfee9b175e4445f19d278de92228e4819806
neilpeter08 <neilpeter_garcia08@yahoo.com>
2014-09-19 18:23:38 +0800

    minor changes

---
+++ html/default/PHP/customize.php
@@ -0,0 +1,27 @@
+
+
+	
+				
+				<div class="main">
+					<a href="#" class="m-btn-grey grey-btn">request a demo</a>
+					<section class="cols">
+						<div class="cl">&nbsp;</div>
+					</section>
+					<section class="cnus">
+					<center>
+					
+					<div class='inline' id='cImage'>
+						<img src="css/shirts/shirt.png">
+					</div>
+					<div class='inline'>adsf</div>
+					<div class='inline hidden box'></div>
+					<div class='inline hidden box'></div>
+					<div class='inline hidden box'></div>
+					<div class='inline hidden box'></div>
+					</center>
+					</section>
+				</div>
+				
+				<!-- end of main -->
+				
+
--- html/default/PHP/mycart.php
+++ html/default/PHP/mycart.php
@@ -1,4 +1,4 @@
-<script type="text/javascript" src="js/script.js"></script>
+
 <?php
 
 	include_once   '../db.php';
@@ -30,6 +30,14 @@
 
 		echo '</div>';
 	}
+	echo'
+	<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+	<input type="hidden" name="cmd" value="_s-xclick">
+	<input type="hidden" name="hosted_button_id" value="7LMBZ8ZN2AXM2">
+	<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+	<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+	</form>';
+
 	}else{
 		echo "<h2>You dont have any orders!</h2>";
 	}
